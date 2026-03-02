b70e95a934700d6f652b3bc83d40790aff522f72
milliearmstrong-glitch <millie.armstrong@student.onslow.school.nz>
2026-03-02 01:07:46 +0000

    im working on it

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,52 +1,32 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+struct Student: Identifiable{
+    let id: UUID = UUID()
+    var name: String
+    var age: Int
+}
+struct Course: CustomStringConvertible{
+    let id: UUID = UUID()
+    var title: String
+    var courseDescription: String
+    var description: String {
+"\(id) \(title) \(courseDescription)"
 
+}
+}
+struct Enrolment: Codable{
+    let studentId: UUID
+    let courseId: UUID
+
+}
 @main
 
 struct SwiftPlayground {
     static func main() {
-func accepts(input: String, isValid: (String) -> Bool) -> Bool {
-    return isValid(input)
-}
-
-let sample = "moonlIght"
-print(accepts(input: sample, isValid: { $0 == $0.lowercased() && $0.count > 8}))
-
-
-
-        let sightings = [
-    (name: "moth", score: 3),
-    (name: "wolf", score: 9),
-    (name: "raven", score: 4),
-    (name: "mist", score: 7),
-    (name: "wisp", score: 2)
-]
-    let M_Ws = sightings.filter { seen in return (seen.name).first == "m" || (seen.name).first == "w"}
-    let Jscores = M_Ws.map{($0.score)}
-    let result = Jscores.reduce(0) { result, jscore in
-    return result + jscore
-    }
-    let maxValue = Jscores.max { $0 < $1}
-    let minValue = Jscores.min { $0 < $1}
-    print (minValue!, maxValue!, result, M_Ws)
-
-
-
-
-        let mixed = ["cat", "7", "owl", "15", "dog", "3"]
-        let pure = mixed.compactMap { Int($0) }
-        print(pure)
-
-        let archive = [
-    [
-        [["candle", "dust"], ["mirror", "ash"]],
-        [["whisper", "shadow"], ["clock", "veil"]]
-    ],
-    [
-        [["stone", "key"], ["relic", "name"]],
-        [["cipher", "bone"], ["ember", "seal"]]
-    ]
-]
-    //let wing = archive.last {shelf in shelf.contains {$0.hasPrefix("e")}}
+let leb = Student(name: "Leb", age: 14)
+let corse1 = Course(title: "how to use after effects, for dumbies", courseDescription: "are you a little node based idot, well then hi leb")
+let studentOne = Enrolment(studentId: leb.id, courseId: corse1.id)
+//let data = try JSONEncoder().encode(lesson)
 }
 }
