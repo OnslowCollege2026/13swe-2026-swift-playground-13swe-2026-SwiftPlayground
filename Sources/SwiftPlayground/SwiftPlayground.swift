7b102b06013f7aa67b8a94bc2406469d53d207cc
oc-Leonard-Truman <139722738+oc-Leonard-Truman@users.noreply.github.com>
2026-03-12 22:12:07 +0000

    changed

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,62 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+import GRDB
+
+/// a reservation at the cafe.
+struct Purchaser : Identifiable, Codable, FetchableRecord, PersistableRecord {
+
+    /// the purchaser Id.
+    let id: Int
+
+    /// the name of the purchaser.
+    var name: String
+
+    /// the amount of people in the purchasers party.
+    var count: Int
+
+    /// the name of the reserved table.
+    var reservedTable: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "PurchaserID"
+        case name = "Name"
+        case count = "Count"
+        case reservedTable = "ReservedTable"
+    }
+}
+struct Order : Identifiable, Codable, FetchableRecord, PersistableRecord {
+    let id: Int
+    let purchaserId: Int
+    var amount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "OrderID"
+        case purchaserId = "PurchaserID"
+        case amount = "Amount"
+    }
+    }
+
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        let dbPath = "Sources/SwiftPlayground/cafe.db"
+        do {
+        let dbQueue = try DatabaseQueue(path: dbPath)
+        print("Database connection successful")
+
+        try dbQueue.read { database in
+            try database.dumpSchema()
+
+            let windowSitter = try Purchaser.find(database, key: [
+                "ReservedTable": "Window Seat"
+            ])
+            print(windowSitter)
+            }
+        } catch {
+            print(error)
+        }
     }
 }
