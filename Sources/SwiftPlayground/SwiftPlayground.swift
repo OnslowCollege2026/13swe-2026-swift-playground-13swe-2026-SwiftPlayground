a0a2aeb389a7fa0e8d22d55bad74a730d887e133
oc-Leonard-Truman <139722738+oc-Leonard-Truman@users.noreply.github.com>
2026-04-22 00:54:50 +0000

    1

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,17 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
 
-@main
-struct SwiftPlayground {
-    static func main() {
-        print("Hello, world!")
-    }
+/// School Library Book Borrowing System
+
+struct book: Identifiable {
+let id: Int
+let name: String
+let genre: String
+let author: String
 }
+@main
+    struct SwiftPlayground {
+    static func main() { 
+    let bookList = book(id: 1, name: "book1", genre: "Sci-fi", author: "Bob Jobs")
+}
+}
+
