d82404c3a6082f6728f687fd0952bf9b0888b486
oc-Leonard-Truman <139722738+oc-Leonard-Truman@users.noreply.github.com>
2026-04-23 10:47:53 +0000

    struct changes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,16 +2,27 @@ import Foundation
 
 /// School Library Book Borrowing System
 
-struct book: Identifiable {
+struct Book: Identifiable {
 let id: Int
 let name: String
-let genre: String
 let author: String
+let availability: Bool
+
+struct user: Identifiable {
+let id: Int
+let name: String
+}
+
+struct loan {
+let bookID: Int
+let userID: Int
+}
+
 }
 @main
     struct SwiftPlayground {
     static func main() { 
-    let bookList = book(id: 1, name: "book1", genre: "Sci-fi", author: "Bob Jobs")
+
 }
 }
 
